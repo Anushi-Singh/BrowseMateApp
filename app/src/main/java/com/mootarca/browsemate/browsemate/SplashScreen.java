8ba2189532025a650fb82510d9a7b25ac2a18fd6
= <apoorvabhishek2010@gmail.com>
2016-09-03 18:56:03 +0530

    Basic Splash Screen

--- app/src/main/java/com/mootarca/browsemate/browsemate/SplashScreen.java
+++ app/src/main/java/com/mootarca/browsemate/browsemate/SplashScreen.java
@@ -1,13 +1,37 @@
 package com.mootarca.browsemate.browsemate;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+    View fullScreenView;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        fullScreenView=findViewById(R.id.brand_name);
+        Typeface tf=Typeface.createFromAsset(getAssets(),"fonts/ARDESTINE.ttf");
+        tv=(TextView)findViewById(R.id.brand_name);
+        tv.setTypeface(tf);
+    }
+    /*This code enables Sticky Immersive screen without clearing flags on swipe.
+    * */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+
+            fullScreenView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 }
